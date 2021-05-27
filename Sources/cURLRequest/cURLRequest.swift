c99b51a8324d8cd3362a329213f4ee6bebce9695
Alexander von Below <Alex@vonBelow.Com>
2021-05-27 13:39:27 +0200

    Added the first curlCommand extension and tests headers

--- Sources/cURLRequest/cURLRequest.swift
+++ Sources/cURLRequest/cURLRequest.swift
@@ -1,2 +1,42 @@
 import Foundation
 
+public extension URLRequest {
+
+    var curlCommand: String {
+        get {
+            var command = "curl "
+
+            guard let method = self.httpMethod?.uppercased() else {
+                return "# No HTTP Method found"
+            }
+            if (method != "GET") {
+                command.append("-X \(method) ")
+            }
+
+            guard let url = self.url?.absoluteString else {
+                return "# No URL found"
+            }
+            command.append(url)
+
+            if let headers = self.allHTTPHeaderFields
+            {
+                for (key, value) in headers {
+                    command.append(" \\\n\t")
+                    command.append("-H '\(key): \(value)'")
+                }
+            }
+
+//            if let bodyData = self.httpBody {
+//                do {
+//                    let jsonObject = try JSONSerialization.jsonObject(with: bodyData, options: [])
+//                    let prettyJSON = try JSONSerialization.data(withJSONObject: jsonObject, options: [.prettyPrinted])
+//                    let prettyString = String(data: prettyJSON, encoding: .utf8)
+//                } catch {
+//
+//                }
+//            }
+
+            return command
+        }
+    }
+}
--- Tests/cURLRequestTests/cURLRequestTests.swift
+++ Tests/cURLRequestTests/cURLRequestTests.swift
@@ -2,4 +2,34 @@
     @testable import cURLRequest
 
     final class cURLRequestTests: XCTestCase {
+        func testSimpleURL() {
+            let urlRequest = URLRequest(url: URL(string: "https://example.com")!)
+
+            let result = urlRequest.curlCommand
+            XCTAssertEqual(result, "curl https://example.com")
+        }
+
+        func testPostURL() {
+            var urlRequest = URLRequest(url: URL(string: "https://example.com")!)
+            urlRequest.httpMethod = "POST"
+
+            let result = urlRequest.curlCommand
+            XCTAssertEqual(result, "curl -X POST https://example.com")
+
+        }
+
+        func testHeadres() {
+            var urlRequest = URLRequest(url: URL(string: "https://example.com")!)
+            urlRequest.setValue("123456", forHTTPHeaderField: "Authorization")
+            urlRequest.setValue("application/x-www-form-urlencoded; charset=utf-8", forHTTPHeaderField: "Content-Type")
+
+            let result = urlRequest.curlCommand
+            let expected1 = "curl https://example.com \\\n\t-H \'Authorization: 123456\' \\\n\t-H \'Content-Type: application/x-www-form-urlencoded; charset=utf-8\'"
+            let expected2 = "curl https://example.com \\\n\t-H \'Content-Type: application/x-www-form-urlencoded; charset=utf-8\' \\\n\t-H \'Authorization: 123456\'"
+
+            let matches1 = expected1 == result
+            let matches2 = expected2 == result
+
+            XCTAssertTrue(matches1 || matches2)
+        }
     }
